17acbc2c130a45230288c703195207922485488b
Mojtaba Safavi <sms7182@gmail.com>
2021-11-16 19:15:34 +0330

    create datatransfer app

---
+++ Client.App/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Client.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var textForSend = string.Empty;
+            while (true)
+            {
+              textForSend=  Console.ReadLine();
+                if (textForSend.ToLower().Contains("exitfatal"))
+                {
+                   break;
+                }
+               await ConnectAndSend(textForSend);
+            }
+        }
+        static async Task ConnectAndSend(string message)
+        {
+
+            try
+            {
+                int port = 7070;
+                TcpClient client = new TcpClient("127.0.0.1", port);
+
+                Byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+
+                
+                NetworkStream stream = client.GetStream();
+              await  stream.WriteAsync(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                data = new Byte[1024];
+                String responseData = String.Empty;
+
+                Int32 bytes =await stream.ReadAsync(data, 0, data.Length);
+                responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+           
+        }
+    }
+}
---
+++ Server.App/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.App
+{
+    class Program
+    {
+        public static string RootDir() => Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @".\"));
+        static async Task Listen()
+        {
+            TcpListener server = null;
+
+            try
+            {
+                int port = 7070;
+                IPAddress local = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(local, port);
+                server.Start();
+              
+                
+                Byte[] bytes = new Byte[1024];
+                String data = null;
+                var prefix = @$"..\..\..\Files\";
+                while (true)
+                {
+
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("Ready for receive message!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    var st=new StringBuilder();
+                    while ((i = await stream.ReadAsync(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                        byte[] msg = System.Text.Encoding.UTF8.GetBytes(data);
+                        await stream.WriteAsync(msg, 0, msg.Length);
+                       
+                       
+                    }
+                   var time= DateTime.UtcNow;
+                   var path = time.ToString("yyyy-MM-dd-tt-HH-mm");
+                    if (time.Second > 30)
+                    {
+                        path += "-30.txt";
+                    }
+                    else
+                    {
+                        path += "-00.txt";
+                    }
+                    var mainPath= Path.GetFullPath(Path.Combine(AppContext.BaseDirectory,prefix+path));
+                    var lines = data.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                  
+                    await File.AppendAllLinesAsync(mainPath, lines);
+
+                    client.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+               
+
+            }
+        }
+        public static async Task CreateFileInfo()
+        {
+            var memory = new ConcurrentDictionary<string, string>();
+            var prefix = @$"..\..\..\Files";
+            var wordRegex = new Regex(@"\w*");
+            while (true)
+            {
+                
+                
+               var files= Directory.GetFiles(prefix);
+                
+                foreach(var file in files)
+                {
+                    if (file.Contains("info"))
+                    {
+                        continue;
+                    }
+                    if (!memory.ContainsKey(file))
+                    {
+                        var builder = new StringBuilder();
+                        var lines= await  File.ReadAllLinesAsync(file);
+                        ConcurrentDictionary<string, int> allWordsInText = new ConcurrentDictionary<string, int>();
+                        builder.AppendLine($"Total Lines:{lines.Length}");
+                        foreach(var line in lines)
+                        {
+                             var matchCollection = wordRegex.Matches(line);
+                             var words=  matchCollection.Cast<Match>().Where(d => d.Success).Where(x => !string.IsNullOrWhiteSpace(x.Value)).GroupBy(x => x.Value).ToDictionary(x => x.Key, e => e.ToList().Count);
+                            foreach(var w in words)
+                            {
+                                if (allWordsInText.ContainsKey(w.Key))
+                                {
+                                    allWordsInText[w.Key] += words[w.Key];
+                                }
+                                else
+                                {
+                                    allWordsInText.TryAdd(w.Key, words[w.Key]);
+                                }
+                            }
+
+                        }
+                        var max = -1;
+                        var maxword = string.Empty;
+                        foreach(var word in allWordsInText)
+                        {
+                            if (word.Value > max)
+                            {
+                                max = word.Value;
+                                maxword = word.Key;
+                            }
+                        }
+
+                        builder.AppendLine($"Max Word:{maxword} with number of:{max}");
+                        
+                        memory.TryAdd(file, file);
+                        builder.AppendLine("Max word in number lines:");
+                       
+                        for(var i= 0; i < lines.Length; i++)
+                        {
+                            if (lines[i].Contains(maxword))
+                            {
+                                builder.Append($"{i + 1},");
+                            }
+                        }
+                        await File.WriteAllTextAsync(file + "-info.txt", builder.ToString());
+                    }
+                    
+                }
+                await Task.Delay(30000);
+            }
+            
+        }
+        static async Task Main(string[] args)
+        {
+            await Task.WhenAll(Listen(), CreateFileInfo());
+        }
+
+    }
+}
